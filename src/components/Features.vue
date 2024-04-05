c2eb276f47e504cf155436fe61386719b1560281
Mwaura Wakati <davewakati@gmail.com>
2024-04-05 18:03:16 +0300

    service section

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -7,76 +7,81 @@
         </div>
       </div>
 
-      <div class="row" style="justify-content:center">
+      <div class="row" style="justify-content:center; padding: 20px">
         <div class="col-md-3" data-aos="fade-up" data-aos-delay="">
-          <div class="feature-1 text-center rounded-lg" style="background-color:grey">
+          <div class="feature-1 text-center rounded-lg" style="background-color:#a5a6a5; display: flex; flex-direction: column-reverse; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-person"></i>
+              <img :src="vaporizer" class="service-image"/>
             </div>
-            <h3 class="mb-3">Get anyone's profile information</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-red">Vaporisateur</h3>
+            <p style="color:black">
+              Apprenez à arrêter de vapoter pour vous aider à réaliser le sevrage en 1 séance.
             </p>
           </div>
         </div>
         <div class="col-md-3" data-aos="fade-up" data-aos-delay="100">
-          <div class="feature-1 text-center">
+          <div class="feature-1 text-center rounded-lg" style="background-color:white; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-search"></i>
+              <img :src="tobacco" class="service-image"/>
             </div>
-            <h3 class="mb-3">Search and explore posts and users</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-red">le tabac</h3>
+            <p style="color:black">
+              Arrêt du tabac avec notre programme et suivi personnalisé
             </p>
           </div>
         </div>
         <div class="col-md-3" data-aos="fade-up" data-aos-delay="200">
-          <div class="feature-1 text-center">
+          <div class="feature-1 text-center rounded-lg" style="background-color:#a5a6a5; display: flex; flex-direction: column-reverse; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-heart"></i>
+              <img :src="can" class="service-image"/>
             </div>
-            <h3 class="mb-3">Get full video information and statistics</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-red">Cannabis</h3>
+            <p style="color:black">
+              Sevrer des drogues douces avec notre programme anti-addiction.
             </p>
           </div>
         </div>
         <div class="col-md-3" data-aos="fade-up" data-aos-delay="">
-          <div class="feature-1 text-center">
+          <div class="feature-1 text-center rounded-lg" style="background-color:white; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-people"></i>
+              <img :src="sugar" class="service-image"/>
             </div>
-            <h3 class="mb-3">Interact with Live streams</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-red">Sucre</h3>
+            <p style="color:black">
+              Régulez votre consommation de sucre avec notre méthode.
             </p>
           </div>
         </div>
         <div class="col-md-3" data-aos="fade-up" data-aos-delay="100">
-          <div class="feature-1 text-center">
+          <div class="feature-1 text-center rounded-lg" style="background-color:#a5a6a5; display: flex; flex-direction: column-reverse; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-app-indicator"></i>
+              <img :src="stress" class="service-image"/>
             </div>
-            <h3 class="mb-3">Get a user's mentions and notifications</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-other">Stresser</h3>
+            <p style="color:black">
+              Gagnez en sérénité avec notre programme contre le stress et l'anxiété.
             </p>
           </div>
         </div>
-        <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
-          <div class="feature-1 text-center">
+        <div class="col-md-3" data-aos="fade-up" data-aos-delay="">
+          <div class="feature-1 text-center rounded-lg" style="background-color:white; padding: 20px; margin-bottom: 20px">
             <div class="wrap-icon icon-1">
-              <i class="bi bi-bell"></i>
+              <img :src="pain" class="service-image"/>
             </div>
-            <h3 class="mb-3">View a user's messages</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
-              optio.
+            <h3 class="mb-3 service-name-other">Douleur</h3>
+            <p style="color:black">
+              Supprimer la douleur avec notre méthode est possible.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3" data-aos="fade-up" data-aos-delay="200">
+          <div class="feature-1 text-center rounded-lg" style="background-color:#a5a6a5; display: flex; flex-direction: column-reverse; padding: 20px; margin-bottom: 20px">
+            <div class="wrap-icon icon-1">
+              <img :src="pain" class="service-image"/>
+            </div>
+            <h3 class="mb-3 service-name-other">Poids</h3>
+            <p style="color:black">
+              Prenez le contrôle de votre poids avec notre accompagnement personnalisé.
             </p>
           </div>
         </div>
@@ -85,9 +90,41 @@
   </section>
 </template>
 <script>
+import tobacco from "@/assets/img/IconCategorie-2-2048x2048.png"
+import sugar from "@/assets/img/IconCategorie-4-2048x2048.png"
+import stress from "@/assets/img/Taterapi-Icon-1-2048x2048.png"
+import pain from "@/assets/img/pain.png"
+import weight from "@/assets/img/weight.png"
+import vaporizer from "@/assets/img/11.png"
+import can from "@/assets/img/IconCategorie-1-2048x2048.png"
 export default {
   data() {
-    return {};
+    return {
+      tobacco:tobacco,
+      sugar:sugar,
+      pain: pain,
+      vaporizer,
+      can,
+      stress,
+      weight
+    };
   },
 };
 </script>
+
+<style scoped>
+.service-image{
+  width: 120px
+}
+
+.service-name-red{
+  color:#f21e1e;
+  font-size: 30px;
+  text-decoration: bold;
+}
+.service-name-other{
+  color:#25652f;
+  font-size: 30px;
+  text-decoration: bold;
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,13 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 mb-4 mb-md-0">
-          <h3>About Adictator</h3>
+          <!--h3>About Adictator</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ea
             delectus pariatur, numquam aperiam dolore nam optio dolorem facilis
             itaque voluptatum recusandae deleniti minus animi.
-          </p>
+          </p-->
+          <img style="width: 200px; height: 200px; margin-bottom: 30px" :src="Logo" />
           <p class="social">
             <router-link to="#" class="social-item"
               ><span class="bi bi-twitter"></span
@@ -27,7 +28,7 @@
         <div class="col-md-7 ms-auto">
           <div class="row site-section pt-0">
             <div class="col-md-4 mb-4 mb-md-0">
-              <h3>Navigation</h3>
+              <h3 style="color:#38B44A">Navigation</h3>
               <ul class="list-unstyled">
                 <li><router-link to="#">Pricing</router-link></li>
                 <li><router-link to="#">Features</router-link></li>
@@ -36,7 +37,7 @@
               </ul>
             </div>
             <div class="col-md-4 mb-4 mb-md-0">
-              <h3>Services</h3>
+              <h3 style="color:#38B44A">Services</h3>
               <ul class="list-unstyled">
                 <li><router-link to="#">Team</router-link></li>
                 <li><router-link to="#">Collaboration</router-link></li>
@@ -45,7 +46,7 @@
               </ul>
             </div>
             <div class="col-md-4 mb-4 mb-md-0">
-              <h3>Downloads</h3>
+              <h3 style="color:#38B44A">Contact</h3>
               <ul class="list-unstyled">
                 <li>
                   <router-link to="#">Get from the App Store</router-link>
@@ -70,19 +71,26 @@
   </footer>
 </template>
 <script>
-export default {};
+import Logo from "@/assets/Logo.jpg";
+export default {
+  data() {
+    return { Logo };
+  },
+};
 </script>
 <style scoped>
 .footer {
   padding: 60px 0;
-  background: #3caec2;
-  color: white;
+  background: white;
+  color: black;
+  border-top: solid;
 }
 .social-item {
-    margin-right: 10px;
+  margin-right: 10px;
+  background-color:#38B44A
 }
 
 .social-item:last-child {
-    margin-right: 0;
+  margin-right: 0;
 }
 </style>
